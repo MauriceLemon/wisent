7937009e6e87e689ff8db604198543e4a2778eb2
MauriceLemon <mauricelemon@my.smccd.edu>
2020-10-08 18:01:30 +0300

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+      <div>
+        <h1>Hello!</h1>
+        <p>Initial setup</p>
+        <Button>Test button!</Button>
+      </div>
+  );
+}
+
+export default App;
